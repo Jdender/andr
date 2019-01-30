fdbf50c00eea0fcd5eba79529f6f372f75924d10
Jdender <jdenderplays@gmail.com>
2019-01-30 18:40:57 -0500

    Add source command

--- src/testing/VoiceTest.ts
+++ src/testing/VoiceTest.ts
@@ -7,7 +7,7 @@ import { Registry } from '../Registry';
 export class VoiceTest {
 
     constructor(
-        client: Client,
+        private client: Client,
         registry: Registry,
     ) {
         registry.addCommand({
@@ -25,15 +25,54 @@ export class VoiceTest {
             usage: 'leave',
             execute: this.leave,
         });
+
+        registry.addCommand({
+            id: 'source',
+            triggers: ['source'],
+            description: 'Brodcast a channel to everyone else.',
+            usage: 'source',
+            execute: this.source,
+        });
+
+        this.brodcast.on('subscribe', () => console.log('Subscribed to brodcast'))
+        this.brodcast.on('unsubscribe', () => console.log('Unsubscribed to brodcast'))
+    }
+
+    private brodcast = this.client.createVoiceBroadcast();
+
+    @bind
+    async source(message: Message) {
+
+        // Find the voice channel the member is in
+        const channel = message.member.voice.channel;
+        if (!channel) return message.channel.send('I can\'t find the voice channel you\'re in, check if you\'re in one.');
+
+        const connection = await channel.join();
+
+        const receiver = connection.receiver;
+
+        await message.channel.send('I\'ve set up an receiver and I\'m ready to brodcast.');
+
+        connection.on('speaking', (user, speaking) => {
+
+            if (!speaking) return;
+
+            const stream = receiver.createStream(user);
+
+            this.brodcast.play(stream);         
+        });
     }
 
     @bind
     async join(message: Message) {
 
+        // Find the voice channel the member is in
         const channel = message.member.voice.channel;
         if (!channel) return message.channel.send('I can\'t find the voice channel you\'re in, check if you\'re in one.');
         
         const connection = await channel.join();
+
+        connection.play(this.brodcast);
 
         return message.channel.send('I joined your voice channel.');
     }
@@ -41,10 +80,11 @@ export class VoiceTest {
     @bind
     async leave(message: Message) {
 
+        // Find the voice channel the bot is in
         const channel = message.guild.me.voice.channel;
         if (!channel) return message.channel.send('I\'m not in a voice channel though.');
 
-        channel.leave();
+        await channel.leave();
 
         return message.channel.send('There, I left.');
     }
