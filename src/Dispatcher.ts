0ed6ea91bad55e15712222985abcdae79a032aaf
Jdender~ <jdenderplays@gmail.com>
2019-03-02 18:01:48 -0500

    Split Dispatcher from Registry

---
+++ src/Dispatcher.ts
@@ -0,0 +1,56 @@
+import { Effect } from './random';
+import { Client, Collection, Message } from 'discord.js';
+import bind from 'bind-decorator';
+import { Registry } from './Registry';
+
+// CONFIG
+const prefix = '`';
+
+@Effect()
+export class Dispatcher {
+
+    constructor(
+        private client: Client,
+        private registry: Registry,
+    ) {
+        client.on('message', this.handle);
+    }
+
+    @bind
+    handle(message: Message) {
+
+        // Basic checks
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+        // Get raw args using regex
+        const args = message.content.slice(prefix.length).split(/\s/g);
+
+        // Get trigger as first arg
+        const trigger = args.shift()!.toLowerCase();
+        if (!trigger) return;
+
+        // Find a command that has the trigger
+        const commandMeta = this.registry.getFromTrigger(trigger);
+        if (!commandMeta) return;
+
+        // Provide extra info for commands
+        const extra: CommandExtra = {
+            prefix,
+            trigger,
+        };
+
+        // Run the command
+        commandMeta.execute(message, args, extra)
+        
+        // Respond with any errors
+        .catch(e => message.channel.send(
+            `There was an error when trying to run that: \`\`\`${e}\`\`\` (You should report this)`,  
+        ));
+    }
+}
+
+// Used to provide extra info to a execute
+export interface CommandExtra {
+    prefix: string;
+    trigger: string;
+}
--- src/Registry.ts
+++ src/Registry.ts
@@ -1,23 +1,10 @@
-import { Effect } from './random';
-import { Client, Collection, Message } from 'discord.js';
-import bind from 'bind-decorator';
+import { Collection, Message } from 'discord.js';
+import { CommandExtra } from './Dispatcher';
 
-// CONFIG
-const prefix = '`';
-
-@Effect()
 export class Registry {
 
     // Store commands
     public commands = new Collection<string, CommandMeta>();
-
-    constructor(
-        private client: Client,
-    ) {
-        client.on('message', this.handle);
-    }
-
-    //-- Register Command --//
 
     addCommand(command: CommandMeta) {
         this.commands.set(command.id, command);
@@ -29,41 +16,6 @@ export class Registry {
             meta => meta.triggers.includes(trigger),
         );
     }
-
-    //-- Handle command --//
-    // (May split into other class)
-
-    @bind
-    handle(message: Message) {
-
-        // Basic checks
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-        // Get raw args using regex
-        const args = message.content.slice(prefix.length).split(/\s/g);
-
-        // Get trigger as first arg
-        const trigger = args.shift()!.toLowerCase();
-        if (!trigger) return;
-
-        // Find a command that has the trigger
-        const commandMeta = this.getFromTrigger(trigger);
-        if (!commandMeta) return;
-
-        // Provide extra info for commands
-        const extra: CommandExtra = {
-            prefix,
-            trigger,
-        };
-
-        // Run the command
-        commandMeta.execute(message, args, extra)
-        
-        // Respond with any errors
-        .catch(e => message.channel.send(
-            `There was an error when trying to run that: \`\`\`${e}\`\`\` (You should report this)`,  
-        ));
-    }
 }
 
 // The info of a command itself
@@ -74,10 +26,4 @@ export interface CommandMeta {
 
     description: string;
     usage: string | string[];
-}
-
-// Used to provide extra info to a execute
-export interface CommandExtra {
-    prefix: string;
-    trigger: string;
 }
