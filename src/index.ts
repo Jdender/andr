866ec98edbfa92fd366399967431d4cd15e5d179
Jdender~ <jdenderplays@gmail.com>
2019-05-16 16:37:37 -0400

    Add typeorm

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,19 @@ import { Container } from 'typedi';
 import { Client } from 'discord.js';
 import { importful } from 'importful';
 import { effectToken } from './random';
+import { createConnection } from 'typeorm';
 
 void async function() {
 
     const client = new Client({});
 
     Container.set(Client, client);
+
+    await createConnection({
+        type: 'sqlite',
+        database: '../.data/db.sqlite',
+        synchronize: true,
+    });
 
     // Register and load services
     await importful(__dirname);
