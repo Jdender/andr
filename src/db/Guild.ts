cfd9baa33b2e0403da0774ae345ccdf3208d2ab9
Jdender~ <jdenderplays@gmail.com>
2019-07-17 15:31:59 -0400

    Add some comments to Guild and GuildConfig

--- src/db/Guild.ts
+++ src/db/Guild.ts
@@ -6,16 +6,22 @@ const defaultConfig = {
     prefix: ',',
 };
 
+// "It's not prototype overloading if it's suported by the library!"
+
 export const Guild = Structures.extend('Guild', Base => class extends Base {
 
     async getConfig(): Promise<GuildConfig> {
 
+        // I don't know if getting a repo every time is good or not
         const repo = getRepository(GuildConfig);
         
         const config = await repo.findOne(this.id);
 
         if (config) return config;
 
+        // If not existing config:
+
+        // Entity's id same as the discord id
         const newConfig: GuildConfig = {
             ...defaultConfig,
             id: this.id,
@@ -30,10 +36,12 @@ export const Guild = Structures.extend('Guild', Base => class extends Base {
 
         const repo = getRepository(GuildConfig);
 
+        // I think save is okay with a Partial
         await repo.save(newConfig);
     }
 });
 
+// Here's some type overloading to go along with the prototype overloading
 declare module 'discord.js' {
 
     interface Guild {
--- src/db/GuildConfig.entity.ts
+++ src/db/GuildConfig.entity.ts
@@ -3,6 +3,7 @@ import { Entity, PrimaryColumn, Column } from 'typeorm';
 @Entity()
 export class GuildConfig {
 
+    // This corresponds do the guild's discord id
     @PrimaryColumn()
     id: string;
 
