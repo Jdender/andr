edbb2ff8519a4b0e3a4addbe1b49591215710d57
Jdender~ <jdenderplays@gmail.com>
2019-07-20 13:40:36 -0400

    Add debug command

---
+++ src/core/Debug.ts
@@ -0,0 +1,67 @@
+import { Client, Message } from 'discord.js';
+import { Effect } from '../random';
+import { Registry } from '../Registry';
+import { CommandContext } from '../Dispatcher';
+import { smallCodeblock, inspectCodeblock } from '../formating';
+
+@Effect()
+export class Debug {
+
+    constructor(
+        private client: Client,
+        registry: Registry,
+    ) {
+        registry.addCommand({
+            id: 'debug',
+            triggers: ['debug'],
+            execute: this.execute,
+            description: 'Debug aspects of the bot',
+            usage: [
+                'debug',
+                `debug <${Object.keys(this.subcommands).join('|')}>`,
+            ],
+        });
+    }
+
+    execute = async (context: CommandContext) => {
+
+        // Don't destruct context in args because we need context for some subcmds
+        const { message, args } = context;
+
+        // Get subcommand from match object
+        const subcommand = this.subcommands[args[0]];
+
+        if (!subcommand) return this.help(message);
+
+        return subcommand(context);
+    }
+
+    // Display subcommands if not a valid one
+    help(message: Message) {
+
+        const help = Object.keys(this.subcommands).map(smallCodeblock).join(', ');
+
+        message.channel.send(
+            `Available debug subcommands are: ${help}`,
+        );
+    }
+
+    subcommands: Subcommands = {
+
+        async guildConfig({ message }) {
+
+            const config = await message.guild.getConfig();
+            
+            message.channel.send(inspectCodeblock(config));
+        },
+
+        commandContext(context) {
+
+            context.message.channel.send(inspectCodeblock(context));
+        },
+    };
+}
+
+interface Subcommands {
+    [subcmd: string]: undefined | ((ctx: CommandContext) => any);
+}
